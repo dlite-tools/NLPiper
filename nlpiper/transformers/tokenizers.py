0edc74dce3e9b00ab309d98411322f59fb5c3994
tomassosorio <tomas.s.osorio@gmail.com>
2021-03-05 14:19:01 +0000

    convert tokenizers to use dataclasses

--- nlpiper/transformers/tokenizers.py
+++ nlpiper/transformers/tokenizers.py
@@ -1,6 +1,8 @@
 """Tokenizer Module."""
 
-from typing import List
+from typing import Union
+
+from nlpiper.core.document import Document, Token
 
 __all__ = ["BasicTokenizer"]
 
@@ -11,20 +13,32 @@ class Tokenizer:
     def __init__(self, **kwargs):
         self.log = kwargs
 
-    def __call__(self, text: str) -> List[str]:
+    def __call__(self, text: Union[str, Document]) -> Document:
         pass
 
 
 class BasicTokenizer(Tokenizer):
     """Basic tokenizer which tokenizes by splitting tokens by blank spaces."""
 
-    def __call__(self, text: str) -> List[str]:
+    def __call__(self, text: Union[str, Document]) -> Document:
         """Tokenize text to list of tokens.
 
         Args:
-            text (str): Text to be tokenized.
+            text (Union[str, Document]): Text to be tokenized.
 
-        Returns: List[str]
+        Returns: Document
         """
-        tokens = text.split()
-        return tokens if tokens is not None else []
+        if isinstance(text, str):
+            doc = Document(text)
+        else:
+            doc = text
+
+        if doc.phrases:
+            phrases = [phrase.split() if phrase is not None else [] for phrase in doc.phrases]
+
+        else:
+            phrases = [doc.cleaned.split() if doc.cleaned is not None else doc.text.split()]
+
+        doc.tokens = [[Token(token) for token in phrase] for phrase in phrases]
+
+        return doc
--- tests/transformers/test_tokenizers.py
+++ tests/transformers/test_tokenizers.py
@@ -1,6 +1,7 @@
 import pytest
 
 from nlpiper.transformers.tokenizers import BasicTokenizer
+from nlpiper.core.document import Document, Token
 
 
 class TestRemovePunctuation:
@@ -10,4 +11,14 @@ class TestRemovePunctuation:
     ])
     def test_remove_punctuation(self, inputs, results):
         t = BasicTokenizer()
-        assert t(inputs) == results
+        input_doc = Document(inputs)
+
+        doc = Document(inputs)
+        doc.tokens = [[Token(token) for token in results]]
+
+        assert t(inputs) == doc
+        assert t(input_doc) == doc
+
+        input_doc.phrases = [inputs]
+        doc.phrases = [inputs]
+        assert t(input_doc) == doc
