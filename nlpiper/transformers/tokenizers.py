4a062ada8931146234be9d42ee2d5b366d94260a
tomassosorio <tomas.s.osorio@gmail.com>
2021-03-10 23:59:31 +0000

    add MosesTokenizer

--- nlpiper/transformers/tokenizers.py
+++ nlpiper/transformers/tokenizers.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from nlpiper.core.document import Document, Token
 
-__all__ = ["BasicTokenizer"]
+__all__ = ["BasicTokenizer", "MosesTokenizer"]
 
 
 class Tokenizer:
@@ -38,6 +38,49 @@ class BasicTokenizer(Tokenizer):
 
         else:
             phrases = [doc.cleaned.split() if doc.cleaned is not None else doc.text.split()]
+
+        doc.tokens = [[Token(token) for token in phrase] for phrase in phrases]
+
+        return doc
+
+
+class MosesTokenizer(Tokenizer):
+    """SacreMoses tokenizer."""
+
+    def __init__(self, **kwargs):
+        """SacreMoses tokenizer.
+
+        Args:
+            **kwargs: See the docs at https://github.com/alvations/sacremoses for more information.
+        """
+        super().__init__(**kwargs)
+        try:
+            from sacremoses import MosesTokenizer
+            self.t = MosesTokenizer()
+
+        except ImportError:
+            print("Please install SacreMoses. "
+                  "See the docs at https://github.com/alvations/sacremoses for more information.")
+            raise
+
+    def __call__(self, text: Union[str, Document]) -> Document:
+        """Tokenize text to list of tokens.
+
+        Args:
+            text (Union[str, Document]): Text to be tokenized.
+
+        Returns: Document
+        """
+        if isinstance(text, str):
+            doc = Document(text)
+        else:
+            doc = text
+
+        if doc.phrases:
+            phrases = [self.t.tokenize(phrase) if phrase is not None else [] for phrase in doc.phrases]
+
+        else:
+            phrases = [self.t.tokenize(doc.cleaned) if doc.cleaned is not None else self.t.tokenize(doc.text)]
 
         doc.tokens = [[Token(token) for token in phrase] for phrase in phrases]
 
