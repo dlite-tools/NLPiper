3085bf6b572758d3bb8f73b8e3d11471ec122da1
Tomás Osório <tomas.s.osorio@gmail.com>
2021-03-11 16:15:45 +0000

    Add moses tokenizer (#3)
    * add MosesTokenizer
    
    * log args and kwargs
    
    * test logs
    
    * improve documentation
    
    * improve documentation
    
    * improve clarity on TestMosesTokenizer

--- nlpiper/transformers/tokenizers.py
+++ nlpiper/transformers/tokenizers.py
@@ -4,14 +4,15 @@ from typing import Union
 
 from nlpiper.core.document import Document, Token
 
-__all__ = ["BasicTokenizer"]
+__all__ = ["BasicTokenizer", "MosesTokenizer"]
 
 
 class Tokenizer:
     """Abstract class to Tokenizers."""
 
-    def __init__(self, **kwargs):
-        self.log = kwargs
+    def __init__(self, *args, **kwargs):
+        args = {"args": list(args)} if len(args) != 0 else {}
+        self.log = {**kwargs, **args}
 
     def __call__(self, text: Union[str, Document]) -> Document:
         raise NotImplementedError
@@ -38,6 +39,53 @@ class BasicTokenizer(Tokenizer):
 
         else:
             phrases = [doc.cleaned.split() if doc.cleaned is not None else doc.text.split()]
+
+        doc.tokens = [[Token(token) for token in phrase] for phrase in phrases]
+
+        return doc
+
+
+class MosesTokenizer(Tokenizer):
+    """SacreMoses tokenizer.
+
+    Transformer to tokenize text using Sacremoses, https://github.com/alvations/sacremoses
+    """
+
+    def __init__(self, *args, **kwargs):
+        """SacreMoses tokenizer.
+
+        Args:
+            *args: See the docs at https://github.com/alvations/sacremoses for more information.
+            **kwargs: See the docs at https://github.com/alvations/sacremoses for more information.
+        """
+        super().__init__(*args, **kwargs)
+        try:
+            from sacremoses import MosesTokenizer
+            self.t = MosesTokenizer(*args, **kwargs)
+
+        except ImportError:
+            print("Please install SacreMoses. "
+                  "See the docs at https://github.com/alvations/sacremoses for more information.")
+            raise
+
+    def __call__(self, text: Union[str, Document]) -> Document:
+        """Tokenize text to list of tokens.
+
+        Args:
+            text (Union[str, Document]): Text to be tokenized.
+
+        Returns: Document
+        """
+        if isinstance(text, str):
+            doc = Document(text)
+        else:
+            doc = text
+
+        if doc.phrases:
+            phrases = [self.t.tokenize(phrase) if phrase is not None else [] for phrase in doc.phrases]
+
+        else:
+            phrases = [self.t.tokenize(doc.cleaned) if doc.cleaned is not None else self.t.tokenize(doc.text)]
 
         doc.tokens = [[Token(token) for token in phrase] for phrase in phrases]
 
--- tests/transformers/test_tokenizers.py
+++ tests/transformers/test_tokenizers.py
@@ -1,7 +1,21 @@
+import builtins
+
 import pytest
 
-from nlpiper.transformers.tokenizers import BasicTokenizer
+from nlpiper.transformers.tokenizers import BasicTokenizer, MosesTokenizer
 from nlpiper.core.document import Document, Token
+
+
+@pytest.fixture
+def hide_available_pkg(monkeypatch):
+    import_orig = builtins.__import__
+
+    def mocked_import(name, *args, **kwargs):
+        if name == 'sacremoses':
+            raise ModuleNotFoundError()
+        return import_orig(name, *args, **kwargs)
+
+    monkeypatch.setattr(builtins, '__import__', mocked_import)
 
 
 class TestBasicTokenizer:
@@ -22,3 +36,41 @@ class TestBasicTokenizer:
         input_doc.phrases = [inputs]
         doc.phrases = [inputs]
         assert t(input_doc) == doc
+
+
+class TestMosesTokenizer:
+    @pytest.mark.parametrize('inputs,results', [
+        ('Test to this test', ['Test', 'to', 'this', 'test']),
+        ('numbers 123 and symbols "#$%', ['numbers', '123', 'and', 'symbols', '&quot;', '#', '$', '%']),
+    ])
+    def test_tokenizer(self, inputs, results):
+        pytest.importorskip('sacremoses')
+        t = MosesTokenizer()
+        input_doc = Document(inputs)
+
+        doc = Document(inputs)
+        doc.tokens = [[Token(token) for token in results]]
+
+        # Given a string as input
+        assert t(inputs) == doc
+
+        # Given a Document as input
+        assert t(input_doc) == doc
+
+        input_doc.phrases = [inputs]
+        doc.phrases = [inputs]
+
+        # Given a Document with phrases as input
+        assert t(input_doc) == doc
+
+    def test_log(self):
+        t = MosesTokenizer(lang="en")
+        assert t.log == {"lang": "en"}
+
+        t = MosesTokenizer("en")
+        assert t.log == {"args": ["en"]}
+
+    @pytest.mark.usefixtures('hide_available_pkg')
+    def test_if_no_package(self):
+        with pytest.raises(ModuleNotFoundError):
+            MosesTokenizer()
