b513327b533d9a37eaefb03faa031a9ca03c1c63
Tomás Osório <tomas.s.osorio@gmail.com>
2021-03-12 09:42:01 +0000

    remove HTML (#4)
    * remove HTML
    
    * improve docs
    
    * add XML test

--- nlpiper/transformers/cleaners.py
+++ nlpiper/transformers/cleaners.py
@@ -102,7 +102,6 @@ class RemovePunctuation(Cleaner):
             text (Union[str, Document]): text to be cleaned.
 
         Returns: Document
-
         """
         if isinstance(text, str):
             doc = Document(text)
@@ -113,4 +112,50 @@ class RemovePunctuation(Cleaner):
             doc.cleaned = doc.text
 
         doc.cleaned = doc.cleaned.translate(str.maketrans('', '', punctuation))
+        return doc
+
+
+class RemoveHTML(Cleaner):
+    """Remove HTML and XML using BeautifulSoup4."""
+
+    def __init__(self, features: str = "html.parser", *args, **kwargs):
+        """Remove HTML and XML.
+
+        Args:
+            features (str): Parser used to remove HTML and XML, which could be used the following parsers:
+            ```"html.parser"```, ```"lxml"```, ```"lxml-xml"```, ```"xml"```,  ```"html5lib"```,
+             for more information about the parser go to: https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+            *args: See the docs at https://www.crummy.com/software/BeautifulSoup/bs4/doc/ for more information.
+            **kwargs: See the docs at https://www.crummy.com/software/BeautifulSoup/bs4/doc/ for more information.
+        """
+        super().__init__(features=features, *args, **kwargs)
+        try:
+            from bs4 import BeautifulSoup
+            self.c = BeautifulSoup
+            self.args = args
+            self.kwargs = kwargs
+            self.features = features
+
+        except ImportError:
+            print("Please install BeautifulSoup4. "
+                  "See the docs at https://www.crummy.com/software/BeautifulSoup/ for more information.")
+            raise
+
+    def __call__(self, text: Union[str, Document]) -> Document:
+        """Remove HTML and XML from text.
+
+        Args:
+            text (Union[str, Document]): text to be cleaned.
+
+        Returns: Document
+        """
+        if isinstance(text, str):
+            doc = Document(text)
+        else:
+            doc = text
+
+        if doc.cleaned is None:
+            doc.cleaned = doc.text
+
+        doc.cleaned = self.c(doc.cleaned, features=self.features, *self.args, **self.kwargs).get_text()
         return doc
--- tests/transformers/test_cleaners.py
+++ tests/transformers/test_cleaners.py
@@ -1,7 +1,21 @@
+import builtins
+
 import pytest
 
-from nlpiper.transformers.cleaners import RemovePunctuation, RemoveUrl, RemoveEmail, RemoveNumber
+from nlpiper.transformers.cleaners import RemovePunctuation, RemoveUrl, RemoveEmail, RemoveNumber, RemoveHTML
 from nlpiper.core.document import Document
+
+
+@pytest.fixture
+def hide_available_pkg(monkeypatch):
+    import_orig = builtins.__import__
+
+    def mocked_import(name, *args, **kwargs):
+        if name == 'bs4':
+            raise ModuleNotFoundError()
+        return import_orig(name, *args, **kwargs)
+
+    monkeypatch.setattr(builtins, '__import__', mocked_import)
 
 
 class TestRemovePunctuation:
@@ -51,7 +65,7 @@ class TestRemoveEmail:
         assert r(Document(inputs)) == doc
 
 
-class TestNumbers:
+class TestRemoveNumber:
     @pytest.mark.parametrize('inputs,results', [
         ('TEST', 'TEST'),
         ('test 12 test', 'test  test'),
@@ -64,3 +78,25 @@ class TestNumbers:
 
         assert r(inputs) == doc
         assert r(Document(inputs)) == doc
+
+
+class TestRemoveHTML:
+    @pytest.mark.parametrize('inputs,results', [
+        ('<html><title>TEST</title>', 'TEST'),
+        ('<p class="title"><b>test 12 test</b></p>', 'test 12 test'),
+        ('<?xml version="1.0" encoding="UTF-8"?><note><body>test123test</body></note>', 'test123test'),
+    ])
+    def test_remove_html(self, inputs, results):
+        pytest.importorskip('bs4')
+        r = RemoveHTML()
+        doc = Document(inputs)
+        doc.cleaned = results
+
+        assert r(inputs) == doc
+        assert r(Document(inputs)) == doc
+        assert r.log == {"features": "html.parser"}
+
+    @pytest.mark.usefixtures('hide_available_pkg')
+    def test_if_no_package(self):
+        with pytest.raises(ModuleNotFoundError):
+            RemoveHTML()
