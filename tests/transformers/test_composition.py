6283e81a1d47ba38a18ea3aeb28c1917ca809065
tomassosorio <tomas.s.osorio@gmail.com>
2021-03-22 13:52:26 +0000

    fix test composition

--- tests/transformers/test_composition.py
+++ tests/transformers/test_composition.py
@@ -19,7 +19,6 @@ class TestCompose:
 
         pipe = Compose([crn, nrp])
 
-        assert pipe(inputs) == doc
         assert pipe(Document(original=inputs)) == doc
 
     @pytest.mark.parametrize('inputs,results', [
@@ -65,5 +64,4 @@ class TestCompose:
             for token, result in zip(phrase, phrase_result):
                 token.processed = result
 
-        assert pipe(inputs) == doc
         assert pipe(input_doc) == doc
