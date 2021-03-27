44e4d50418c88d9884561646c1daf24e7a2172c2
Tomas Osorio <tomas.s.osorio@gmail.com>
2021-03-27 22:21:13 +0000

    add tests for full coverage

--- tests/transformers/test_normalizers.py
+++ tests/transformers/test_normalizers.py
@@ -1,9 +1,22 @@
+import builtins
 from copy import deepcopy
 
 import pytest
 
 from nlpiper.transformers.normalizers import CaseTokens, RemovePunctuation, RemoveStopWords
 from nlpiper.core.document import Document, Token
+
+
+@pytest.fixture
+def hide_available_pkg(monkeypatch):
+    import_orig = builtins.__import__
+
+    def mocked_import(name, *args, **kwargs):
+        if name == 'nltk':
+            raise ModuleNotFoundError()
+        return import_orig(name, *args, **kwargs)
+
+    monkeypatch.setattr(builtins, '__import__', mocked_import)
 
 
 class TestCaseTokens:
@@ -105,6 +118,27 @@ class TestRemoveStopWords:
 
         assert n(input_doc) == doc
 
+    @pytest.mark.parametrize('inputs,results', [
+    ([['This', 'is', 'a', 'stop', 'Word']], [['', '', '', 'stop', 'Word']]),])
+    def test_remove_stop_words_wo_case_sensitive(self, inputs, results):
+        pytest.importorskip('nltk')
+        n = RemoveStopWords(case_sensitive=False)
+
+        # Prepare input doc
+        phrases = [" ".join(phrase) for phrase in inputs]
+        doc = Document(original=" ".join(phrases))
+        doc.cleaned = doc.original
+        doc.phrases = phrases
+        doc.tokens = [[Token(original=token) for token in phrase] for phrase in inputs]
+        input_doc = deepcopy(doc)
+
+        # Prepare result doc
+        for phrase, phrase_result in zip(doc.tokens, results):
+            for token, result in zip(phrase, phrase_result):
+                token.cleaned = result
+
+        assert n(input_doc) == doc
+
     @pytest.mark.parametrize('inputs', ["string", 2])
     def test_with_invalid_document(self, inputs):
         pytest.importorskip('nltk')
@@ -120,3 +154,8 @@ class TestRemoveStopWords:
         with pytest.raises(TypeError):
             n = RemoveStopWords()
             n(Document(original=inputs))
+
+    @pytest.mark.usefixtures('hide_available_pkg')
+    def test_if_no_package(self):
+        with pytest.raises(ModuleNotFoundError):
+            RemoveStopWords()
