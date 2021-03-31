30d43d0856a22c71f581895465ea6b96ede264c4
Daniel Ferrari <ferrari.dg@gmail.com>
2021-03-31 19:41:20 +0100

    Refactor document - Add constructors - Remove phrases attribute - Add decorator functions

--- nlpiper/core/document.py
+++ nlpiper/core/document.py
@@ -1,5 +1,4 @@
 from typing import List, Optional
-
 from pydantic import BaseModel
 
 
@@ -9,10 +8,43 @@ class Token(BaseModel):
     lemma: Optional[str] = None
     stem: Optional[str] = None
 
+    def __init__(self, original: str, **data) -> None:
+        super().__init__(original=original, cleaned=original, **data)
+
 
 class Document(BaseModel):
     original: str
-    cleaned: Optional[str] = None
-    phrases: Optional[List[str]] = None
-    tokens: Optional[List[List[Token]]] = None
+    cleaned: str
+    tokens: Optional[List[Token]] = None
     steps: List[str] = []
+
+    def __init__(self, original: str, **data) -> None:
+        super().__init__(original=original, cleaned=original, **data)
+
+
+# Decorators
+def validate_doc(func):
+    """Validate function first argument as a Document instance."""
+
+    def wrapper(*args, **kwargs):
+        if not isinstance(args[1], Document):
+            raise TypeError("Argument doc is not of type Document")
+        out = func(*args, **kwargs)
+        return out
+
+    return wrapper
+
+
+def add_step(func):
+    """Register a transformation into the document object."""
+
+    def wrapper(*args, **kwargs):
+        out = func(*args, **kwargs)
+        if out is None:
+            args[1].steps.append(repr(args[0]))
+        else:
+            out.steps.append(repr(args[0]))
+
+        return out
+
+    return wrapper
