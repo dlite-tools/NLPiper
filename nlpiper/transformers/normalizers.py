b1cd5af99763adc87ac142278967cb8cac2b1695
Daniel Ferrari <ferrari.dg@gmail.com>
2021-04-05 13:43:24 +0100

    Refactor normalizers and unit tests

--- nlpiper/transformers/normalizers.py
+++ nlpiper/transformers/normalizers.py
@@ -1,92 +1,83 @@
 """Normalizer Module."""
 
 from string import punctuation
+from typing import Optional
 
 from nlpiper.core.document import Document
-from nlpiper.transformers import BaseTransformer
+from nlpiper.transformers import (
+    BaseTransformer,
+    TransformersType,
+    add_step,
+    validate
+)
+from nlpiper.logger import log
 
-__all__ = ["CaseTokens", "RemovePunctuation", "RemoveStopWords"]
-
-
-class Normalizer(BaseTransformer):
-    """Abstract class to Normalizers."""
-
-    def _validate_document(self, doc: Document):
-        """Validate if document is ready to be processed.
-
-        Args:
-            doc (Document): document to be cleaned.
-
-        Raises:
-            TypeError: if doc is not a Document.
-        """
-        if not isinstance(doc, Document):
-            raise TypeError("Argument doc is not of type Document")
-
-        if doc.cleaned is None:
-            doc.cleaned = doc.original
-
-        if doc.phrases is None:
-            doc.phrases = doc.cleaned
-
-        if doc.tokens is None:
-            raise TypeError("Document does not contain tokens.")
-
-        for phrase in doc.tokens:
-            for token in phrase:
-                if token.cleaned is None:
-                    token.cleaned = token.original
+__all__ = [
+    "CaseTokens",
+    "RemovePunctuation",
+    "RemoveStopWords"
+]
 
 
-class CaseTokens(Normalizer):
-    """Lower tokens."""
+class CaseTokens(BaseTransformer):
+    """Uppercase or Lowercase tokens."""
 
     def __init__(self, mode='lower'):
-        """Lower tokens.
+        """Case tokens.
 
         Args:
             mode (str): Mode can be ```'lower'``` or ```'upper'```, lowering or upper casing the letters respectively.
         """
+        if mode not in ('upper', 'lower'):
+            raise ValueError(f'{mode} mode is not available, it can only be "upper" or "lower".')
+
         super().__init__(mode=mode)
-        assert mode in ('upper', 'lower'), f'{mode} mode is not available, it can only be "upper" or "lower".'
         self.mode = mode
 
-    def __call__(self, doc: Document) -> Document:
-        """Lower Tokens.
+    @validate(TransformersType.NORMALIZERS)
+    @add_step
+    def __call__(self, doc: Document, inplace: bool = False) -> Optional[Document]:
+        """Uppercase or Lowercase tokens.
 
         Args:
             doc (Document): Document to be normalized.
+            inplace (bool): if True will return a new doc object,
+                            otherwise will change the object passed as parameter.
 
         Returns: Document
         """
-        super()._validate_document(doc)
+        d = doc if inplace else doc._deepcopy()
 
-        for phrase in doc.tokens:
-            for token in phrase:
-                token.cleaned = getattr(token.cleaned, self.mode)()
-        return doc
+        for token in d.tokens:
+            token.cleaned = getattr(token.cleaned, self.mode)()
+
+        return None if inplace else d
 
 
-class RemovePunctuation(Normalizer):
+class RemovePunctuation(BaseTransformer):
     """Remove Punctuation."""
 
-    def __call__(self, doc: Document) -> Document:
+    @validate(TransformersType.NORMALIZERS)
+    @add_step
+    def __call__(self, doc: Document, inplace: bool = False) -> Optional[Document]:
         """Remove punctuation.
 
         Args:
             doc (Document): Document to be normalized.
+            inplace (bool): if True will return a new doc object,
+                            otherwise will change the object passed as parameter.
 
         Returns: Document
         """
-        super()._validate_document(doc)
+        d = doc if inplace else doc._deepcopy()
 
-        for phrase in doc.tokens:
-            for token in phrase:
-                token.cleaned = token.cleaned.translate(str.maketrans('', '', punctuation))
-        return doc
+        for token in d.tokens:
+            token.cleaned = token.cleaned.translate(str.maketrans('', '', punctuation))
+
+        return None if inplace else d
 
 
-class RemoveStopWords(Normalizer):
+class RemoveStopWords(BaseTransformer):
     """Remove Stop Words."""
 
     def __init__(self, language: str = "english", case_sensitive: bool = True):
@@ -108,22 +99,25 @@ class RemoveStopWords(Normalizer):
             self.stopwords = nltk.corpus.stopwords.words(language)
 
         except ImportError:
-            print("Please install NLTK. "
-                  "See the docs at https://www.nltk.org/install.html for more information.")
+            log.error("Please install NLTK. "
+                      "See the docs at https://www.nltk.org/install.html for more information.")
             raise
 
-    def __call__(self, doc: Document) -> Document:
+    @validate(TransformersType.NORMALIZERS)
+    @add_step
+    def __call__(self, doc: Document, inplace: bool = False) -> Optional[Document]:
         """Remove Stop Words.
 
         Args:
             doc (Document): Document to be normalized.
+            inplace (bool): if True will return a new doc object,
+                            otherwise will change the object passed as parameter.
 
         Returns: Document
         """
-        super()._validate_document(doc)
+        d = doc if inplace else doc._deepcopy()
 
-        for phrase in doc.tokens:
-            for token in phrase:
-                token.cleaned = "" if getattr(token.cleaned, self.case_sensitive)() in self.stopwords else token.cleaned
+        for token in d.tokens:
+            token.cleaned = "" if getattr(token.cleaned, self.case_sensitive)() in self.stopwords else token.cleaned
 
-        return doc
+        return None if inplace else d
--- tests/transformers/test_normalizers.py
+++ tests/transformers/test_normalizers.py
@@ -1,10 +1,17 @@
 import builtins
-from copy import deepcopy
 
 import pytest
 
-from nlpiper.transformers.normalizers import CaseTokens, RemovePunctuation, RemoveStopWords
-from nlpiper.core.document import Document, Token
+from nlpiper.transformers.normalizers import (
+    CaseTokens,
+    RemovePunctuation,
+    RemoveStopWords
+)
+from nlpiper.transformers.tokenizers import BasicTokenizer
+from nlpiper.core.document import (
+    Document,
+    Token
+)
 
 
 @pytest.fixture
@@ -12,150 +19,141 @@ def hide_available_pkg(monkeypatch):
     import_orig = builtins.__import__
 
     def mocked_import(name, *args, **kwargs):
-        if name == 'nltk':
+        if name in ('nltk'):
             raise ModuleNotFoundError()
         return import_orig(name, *args, **kwargs)
 
     monkeypatch.setattr(builtins, '__import__', mocked_import)
 
 
-class TestCaseTokens:
-    @pytest.mark.parametrize('mode,inputs,results', [
-        ('lower', [['TEST']], [['test']]),
-        ('upper', [['test']], [['TEST']]),
-    ])
-    def test_modes(self, mode, inputs, results):
-        n = CaseTokens(mode)
-
-        # Prepare input doc
-        phrases = [" ".join(phrase) for phrase in inputs]
-        doc = Document(original=" ".join(phrases))
-        doc.cleaned = doc.original
-        doc.phrases = phrases
-        doc.tokens = [[Token(original=token) for token in phrase] for phrase in inputs]
-        input_doc = deepcopy(doc)
-
-        # Prepare result doc
-        for phrase, phrase_result in zip(doc.tokens, results):
-            for token, result in zip(phrase, phrase_result):
-                token.cleaned = result
-
-        assert n(input_doc) == doc
-
-    @pytest.mark.parametrize('mode', [1, 'other'])
-    def test_non_existent_mode(self, mode):
-        with pytest.raises(AssertionError):
-            CaseTokens(mode)
+class TestNormalizersValidations:
 
     @pytest.mark.parametrize('inputs', ["string", 2])
-    def test_with_invalid_document(self, inputs):
+    def test_with_invalid_input(self, inputs):
         with pytest.raises(TypeError):
-            n = CaseTokens()
-            n(inputs)
+            t = CaseTokens()
+            t(inputs)
 
-    @pytest.mark.parametrize('inputs', ["string", 123])
-    def test_document_wo_tokens(self, inputs):
-        with pytest.raises(TypeError):
-            n = CaseTokens()
-            n(Document(original=inputs))
+    @pytest.mark.parametrize('inputs', ["test"])
+    def test_without_doc_tokens(self, inputs):
+        doc = Document("test")
 
-
-class TestRemovePunctuation:
-    @pytest.mark.parametrize('inputs,results', [
-        ([['TEST.%$#"#']], [['TEST']]),
-        ([[r'!"te""!"#$%&()*+,-.s/:;<=>?@[\]^_`{|}~""t']], [['test']]),
-    ])
-    def test_remove_punctuation(self, inputs, results):
-        n = RemovePunctuation()
-
-        # Prepare input doc
-        phrases = [" ".join(phrase) for phrase in inputs]
-        doc = Document(original=" ".join(phrases))
-        doc.cleaned = doc.original
-        doc.phrases = phrases
-        doc.tokens = [[Token(original=token) for token in phrase] for phrase in inputs]
-        input_doc = deepcopy(doc)
-
-        # Prepare result doc
-        for phrase, phrase_result in zip(doc.tokens, results):
-            for token, result in zip(phrase, phrase_result):
-                token.cleaned = result
-
-        assert n(input_doc) == doc
-
-    @pytest.mark.parametrize('inputs', ["string", 2])
-    def test_with_invalid_document(self, inputs):
-        with pytest.raises(TypeError):
-            n = RemovePunctuation()
-            n(inputs)
-
-    @pytest.mark.parametrize('inputs', ["string", 123])
-    def test_document_wo_tokens(self, inputs):
-        with pytest.raises(TypeError):
-            n = RemovePunctuation()
-            n(Document(original=inputs))
-
-
-class TestRemoveStopWords:
-    @pytest.mark.parametrize('inputs,results', [
-        ([['This', 'is', 'a', 'stop', 'Word']], [['This', '', '', 'stop', 'Word']]),])
-    def test_remove_stop_words_w_case_sensitive(self, inputs, results):
-        pytest.importorskip('nltk')
-        n = RemoveStopWords(case_sensitive=True)
-
-        # Prepare input doc
-        phrases = [" ".join(phrase) for phrase in inputs]
-        doc = Document(original=" ".join(phrases))
-        doc.cleaned = doc.original
-        doc.phrases = phrases
-        doc.tokens = [[Token(original=token) for token in phrase] for phrase in inputs]
-        input_doc = deepcopy(doc)
-
-        # Prepare result doc
-        for phrase, phrase_result in zip(doc.tokens, results):
-            for token, result in zip(phrase, phrase_result):
-                token.cleaned = result
-
-        assert n(input_doc) == doc
-
-    @pytest.mark.parametrize('inputs,results', [
-    ([['This', 'is', 'a', 'stop', 'Word']], [['', '', '', 'stop', 'Word']]),])
-    def test_remove_stop_words_wo_case_sensitive(self, inputs, results):
-        pytest.importorskip('nltk')
-        n = RemoveStopWords(case_sensitive=False)
-
-        # Prepare input doc
-        phrases = [" ".join(phrase) for phrase in inputs]
-        doc = Document(original=" ".join(phrases))
-        doc.cleaned = doc.original
-        doc.phrases = phrases
-        doc.tokens = [[Token(original=token) for token in phrase] for phrase in inputs]
-        input_doc = deepcopy(doc)
-
-        # Prepare result doc
-        for phrase, phrase_result in zip(doc.tokens, results):
-            for token, result in zip(phrase, phrase_result):
-                token.cleaned = result
-
-        assert n(input_doc) == doc
-
-    @pytest.mark.parametrize('inputs', ["string", 2])
-    def test_with_invalid_document(self, inputs):
-        pytest.importorskip('nltk')
-
-        with pytest.raises(TypeError):
-            n = RemoveStopWords()
-            n(inputs)
-
-    @pytest.mark.parametrize('inputs', ["string", 123])
-    def test_document_wo_tokens(self, inputs):
-        pytest.importorskip('nltk')
-
-        with pytest.raises(TypeError):
-            n = RemoveStopWords()
-            n(Document(original=inputs))
+        t = CaseTokens()
+        with pytest.raises(RuntimeError):
+            t(doc)
 
     @pytest.mark.usefixtures('hide_available_pkg')
     def test_if_no_package(self):
         with pytest.raises(ModuleNotFoundError):
             RemoveStopWords()
+
+
+class TestCaseTokens:
+    @pytest.mark.parametrize('mode,inputs,results', [
+        ('lower', ['TEST'], ['test']),
+        ('lower', ['test'], ['test']),
+        ('upper', ['test'], ['TEST']),
+        ('upper', ['TEST'], ['TEST']),
+    ])
+    def test_modes(self, mode, inputs, results):
+
+        results_expected = [Token(tk) for tk in inputs]
+        for tk, out in zip(results_expected, results):
+            tk.cleaned = out
+
+        doc = Document(" ".join(inputs))
+
+        # To apply a normalizer is necessary to have tokens
+        t = BasicTokenizer()
+        t(doc, inplace=True)
+
+        n = CaseTokens(mode)
+
+        # Inplace False
+        out = n(doc)
+
+        assert out.tokens == results_expected
+        assert out.steps == [repr(t), repr(n)]
+        assert doc.tokens == [Token(token) for token in inputs]
+        assert doc.steps == [repr(t)]
+
+        # Inplace True
+        out2 = n(doc, True)
+
+        assert doc.tokens == results_expected
+        assert doc.steps == [repr(t), repr(n)]
+        assert out2 is None
+
+    @pytest.mark.parametrize('mode', [1, 'other'])
+    def test_non_existent_mode(self, mode):
+        with pytest.raises(ValueError):
+            CaseTokens(mode)
+
+
+class TestRemovePunctuation:
+    @pytest.mark.parametrize('inputs,results', [
+        (['TEST.%$#"#'], ['TEST']),
+        ([r'!"te""!"#$%&()*+,-.s/:;<=>?@[\]^_`{|}~""t'], ['test']),
+    ])
+    def test_remove_punctuation(self, inputs, results):
+
+        results_expected = [Token(tk) for tk in inputs]
+        for tk, out in zip(results_expected, results):
+            tk.cleaned = out
+
+        doc = Document(" ".join(inputs))
+
+        # To apply a normalizer is necessary to have tokens
+        t = BasicTokenizer()
+        t(doc, inplace=True)
+
+        n = RemovePunctuation()
+        # Inplace False
+        out = n(doc)
+
+        assert out.tokens == results_expected
+        assert out.steps == [repr(t), repr(n)]
+        assert doc.tokens == [Token(token) for token in inputs]
+        assert doc.steps == [repr(t)]
+
+        # Inplace True
+        out = n(doc, True)
+
+        assert doc.tokens == results_expected
+        assert doc.steps == [repr(t), repr(n)]
+        assert out is None
+
+
+class TestRemoveStopWords:
+    @pytest.mark.parametrize('sensitive,inputs,results', [
+        (True, ['This', 'is', 'a', 'stop', 'Word'], ['This', '', '', 'stop', 'Word']),
+        (False, ['This', 'is', 'a', 'stop', 'Word'], ['', '', '', 'stop', 'Word']),
+    ])
+    def test_remove_stop_words_w_case_sensitive(self, sensitive, inputs, results):
+        pytest.importorskip('nltk')
+
+        results_expected = [Token(tk) for tk in inputs]
+        for tk, out in zip(results_expected, results):
+            tk.cleaned = out
+
+        doc = Document(" ".join(inputs))
+
+        # To apply a normalizer is necessary to have tokens
+        t = BasicTokenizer()
+        t(doc, inplace=True)
+
+        n = RemoveStopWords(case_sensitive=sensitive)
+        # Inplace False
+        out = n(doc)
+
+        assert out.tokens == results_expected
+        assert out.steps == [repr(t), repr(n)]
+        assert doc.tokens == [Token(token) for token in inputs]
+        assert doc.steps == [repr(t)]
+
+        # Inplace True
+        out = n(doc, True)
+
+        assert doc.tokens == results_expected
+        assert doc.steps == [repr(t), repr(n)]
+        assert out is None
