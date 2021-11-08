7b4df783cb3ed5e884f44f1198886ce809177d76
tomasosorio <tomas.s.osorio@gmail.com>
2021-11-08 22:21:20 +0000

    feat: add stanza tokenizer

--- tests/transformers/test_tokenizers.py
+++ tests/transformers/test_tokenizers.py
@@ -4,7 +4,8 @@ import pytest
 
 from nlpiper.transformers.tokenizers import (
     BasicTokenizer,
-    MosesTokenizer
+    MosesTokenizer,
+    StanzaTokenizer
 )
 from nlpiper.core.document import (
     Document,
@@ -83,6 +84,33 @@ class TestMosesTokenizer:
 
         doc = Document(inputs)
         t = MosesTokenizer()
+
+        # Inplace False
+        out = t(doc)
+
+        assert out.tokens == [Token(token) for token in results]
+        assert out.steps == [repr(t)]
+        assert doc.tokens is None
+        assert doc.steps == []
+
+        # Inplace True
+        out = t(doc, True)
+
+        assert doc.tokens == [Token(token) for token in results]
+        assert doc.steps == [repr(t)]
+        assert out is None
+
+
+class TestStanzaTokenizer:
+    @pytest.mark.parametrize('inputs,results', [
+        ('Test to this test', ['Test', 'to', 'this', 'test']),
+        ('numbers 123 and symbols "#$%', ['numbers', '123', 'and', 'symbols', '"#$%']),
+    ])
+    def test_tokenizer(self, inputs, results):
+        pytest.importorskip('stanza')
+
+        doc = Document(inputs)
+        t = StanzaTokenizer()
 
         # Inplace False
         out = t(doc)
