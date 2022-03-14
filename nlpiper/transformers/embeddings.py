392db6749020d47459852ed5d588271dbe0a6667
jfecunha <joao.cunha@wolterskluwer.com>
2022-03-14 10:31:32 +0000

    feat: add TorchTextEmbeddings

--- nlpiper/transformers/embeddings.py
+++ nlpiper/transformers/embeddings.py
@@ -1,6 +1,9 @@
 """Embeddings Module."""
 
-from typing import Any, Optional
+from typing import (
+    Any,
+    Optional
+)
 
 from nlpiper.core.document import Document
 from nlpiper.transformers.base import (
@@ -12,23 +15,49 @@ from nlpiper.transformers.base import (
 from nlpiper.logger import log
 
 __all__ = [
-    "GensimEmbeddings"
+    "GensimEmbeddings",
+    "TorchTextEmbeddings"
 ]
 
 
 class GensimEmbeddings(BaseTransformer):
-    """Gensim Embedding extraction."""
+    """Gensim Embedding extraction.
+
+    Callable arguments:
+
+    Args:
+            keyed_vectors (Any): Gensim model based on keyedVectors,
+                see more in: https://radimrehurek.com/gensim/models/keyedvectors.html
+            apply_doc (str): Mode to calculate the embeddings vector for the document,
+                which could be `"mean"` or `"sum"` of the tokens.
+
+    Returns: Document
+
+    Example:
+        import gensim.downloader
+
+        from nlpiper.transformers.embeddings import GensimEmbeddings
+        from nlpiper.core.document import Document
+        from nlpiper.transformers.tokenizers import BasicTokenizer
+
+        glove_vectors = gensim.downloader.load('glove-twitter-25')
+        doc = Document('Test random stuff.')
+
+        t = BasicTokenizer()
+        t(doc, inplace=True)
+
+        e = GensimEmbeddings(glove_vectors, True)
+
+    """
 
     def __init__(self, keyed_vectors: Any, apply_doc: str = 'mean'):
-        """Stem tokens.
-
-        Gensim Embedding extraction.
+        """ Gensim Embedding extraction.
 
         Args:
             keyed_vectors (Any): Gensim model based on keyedVectors,
-            see more in: https://radimrehurek.com/gensim/models/keyedvectors.html
+                see more in: https://radimrehurek.com/gensim/models/keyedvectors.html
             apply_doc (str): Mode to calculate the embeddings vector for the document,
-            which could be `"mean"` or `"sum"` of the tokens.
+                which could be `"mean"` or `"sum"` of the tokens.
         """
         super().__init__(keyed_vectors=keyed_vectors, apply_doc=apply_doc)
         assert apply_doc in ('sum', 'mean'), 'apply_doc value is not valid, can only be: `"mean"` or `"sum"`.'
@@ -76,5 +105,94 @@ class GensimEmbeddings(BaseTransformer):
 
         d.embedded = (getattr(self.np, self.apply_doc)([token.embedded for token in d.tokens], axis=0)
                       if len(d.tokens) != 0 else self.np.zeros(self.keyed_vectors.vector_size))
+
+        return None if inplace else d
+
+
+class TorchTextEmbeddings(BaseTransformer):
+    """Torch Embeddings extraction.
+
+    Callable arguments:
+
+    Args:
+        vectors (Any): Torchtext embedding model. Available models: [`Glove`, `CharNGram`, `FastText`].
+            Check further info here: https://pytorch.org/text/stable/vocab.html#pretrained-word-embeddings
+        apply_doc (str): Mode to calculate the embeddings vector for the document,
+            which could be `"mean"` or `"sum"` of the tokens.
+
+    Returns: Document
+
+     Example:
+
+        from torchtext.vocab import CharNGram
+
+        from nlpiper.transformers.embeddings import TorchTextEmbeddings
+        from nlpiper.core.document import Document
+        from nlpiper.transformers.tokenizers import BasicTokenizer
+
+        doc = Document('Test random stuff.')
+
+        t = BasicTokenizer()
+        t(doc, inplace=True)
+
+        model = CharNGram()
+        e = TorchTextEmbeddings(model, True)
+
+    """
+
+    def __init__(self, model: Any, apply_doc: str = 'mean', **kwargs):
+        """Torch Embeddings extraction.
+
+        Args:
+            vectors (Any): Torchtext embedding model. Available models: [`Glove`, `CharNGram`, `FastText`].
+                Check further info here: https://pytorch.org/text/stable/vocab.html#pretrained-word-embeddings
+            apply_doc (str): Mode to calculate the embeddings vector for the document,
+                which could be `"mean"` or `"sum"` of the tokens.
+
+        """
+        super().__init__(model=model, apply_doc=apply_doc, **kwargs)
+        assert apply_doc in ('sum', 'mean'), 'apply_doc value is not valid, can only be: `"mean"` or `"sum"`.'
+
+        try:
+            import numpy as np
+            self.np = np
+        except ImportError:
+            log.error("To use embeddings please install numpy. "
+                      "See the docs at https://numpy.org/ for more information.")
+            raise
+
+        try:
+            import torchtext
+            assert issubclass(model.__class__, torchtext.vocab.Vectors), 'vectors is not of type `Vectors`'
+            self.model = model
+            self.apply_doc = apply_doc
+
+        except ImportError:
+            log.error("Please install torchtext. "
+                      "See the docs at https://pytorch.org/text/stable/index.html for more information.")
+            raise
+        self.kwargs['vector_size'] = self.model.dim
+
+    @validate(TransformersType.EMBEDDINGS)
+    @add_step
+    def __call__(self, doc: Document, inplace: bool = False) -> Optional[Document]:
+        """Torch Embeddings extraction.
+
+        Args:
+            doc (Document): Document to be normalized.
+            inplace (bool): if False will return a new doc object,
+                            otherwise will change the object passed as parameter.
+
+        Returns: Document
+        """
+        d = doc if inplace else doc._deepcopy()
+
+        for token in d.tokens:
+
+            token.embedded = self.model.get_vecs_by_tokens(
+                token.cleaned).reshape(-1).to('cpu').detach().numpy()
+
+        d.embedded = (getattr(self.np, self.apply_doc)([token.embedded for token in d.tokens], axis=0)
+                      if len(d.tokens) != 0 else self.np.zeros(self.model.dim))
 
         return None if inplace else d
